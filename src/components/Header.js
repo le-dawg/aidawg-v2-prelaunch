34e061cb214b1ac7b9045169da6f1a421ad01574
le-dawg <dawid.golebiewski@tuhh.de>
2020-08-26 03:52:06 -0700

    lovo pre3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import config from '../../config';
+import LogoImg from '../assets/img/aidawglogov2.svg';
 
 export default function Footer() {
   return (
     <header id="header">
       <h1>{config.heading}</h1>
-      <object type="image/svg+xml" data="../assets/img/aidawglogov2.svg" class="logo"></object>
+      <object type="image/svg+xml" data={LogoImg} class="logo"></object>
       <p>{config.subHeading}</p>
     </header>
   );
